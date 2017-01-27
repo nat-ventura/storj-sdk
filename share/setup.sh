212ca637b005aabac29aee513a9eb4aa88115f98
retrohacker <william.jblankenship@gmail.com>
2017-01-27 14:30:01 -0500

    Allows farmers to scale while maintaining state

--- share/setup.sh
+++ share/setup.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
 
-# Not sure if we will use this or not... simply an experiment
+DIRECTORY="/root/.storjshare"
 
 # Fetch the IP Address of the container
 IP=$(ip addr show dev eth0 | grep 'inet ' | sed 's/\// /g' | awk '{ print $2 }')
 
-# Add it to config
-sed "s/{{ IP_ADDRESS }}/$IP/g" /root/.storjshare/config.template.json > /root/config.json
+for i in {0..10000}; do
+  DIR="$DIRECTORY/farmer_$i"
+  if [ ! -d "$DIR" ]; then
+    echo "Creating ${DIR}..."
+    mkdir -p "$DIR"
+  fi
+  FILE="$DIR/.claim"
+  echo "Checking if $DIR is claimed..."
+  if lockfile -r 0 "$DIR/.claim"; then
+    echo "Claimed $DIR!"
+    break
+  fi
+  echo "$DIR claimed, trying next..."
+done
 
-exec $@
+trap "{ echo 'Cleaning up $FILE'; rm -rf $FILE; }" EXIT
+
+# Add it to config
+cat /root/.storjshare/config.template.json | sed "s/{{ IP_ADDRESS }}/$IP/g" | sed "s~{{ STORAGE_PATH }}~$DIR~g" > /root/config.json
+
+/bin/bash -c "$@"
