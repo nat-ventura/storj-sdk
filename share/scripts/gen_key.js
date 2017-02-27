4cc85f7446a8c785ac70f0769b5168d870b9c1a7
retrohacker <william.jblankenship@gmail.com>
2017-02-27 14:54:34 -0500

    Refactor and add comments

--- share/scripts/gen_key.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-var storj = require('/usr/src/app/node_modules/storj-lib');
-
-var key = storj.KeyPair().getPrivateKey();
-
-//fs.writeFileSync('/opt/storj/.storjshare/id_ecdsa', encryptedKey);
-
-console.log('%s', key);
