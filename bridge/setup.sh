31a3556f685063c57e54b7e1bb59838a599288e2
Philip Hutchins <flipture@gmail.com>
2017-04-25 11:16:04 -0400

    updating bridge install dir

--- bridge/setup.sh
+++ bridge/setup.sh
@@ -1,11 +1,11 @@
-cd /usr/src/app
+cd /storj/bridge
 
 for dir in /usr/src/vendor/* ; do
   if [[ -d $dir ]]; then
     echo "Manually linking $dir"
     dir_name=$(basename $dir)
-    rm -rf /usr/src/app/node_modules/$dir_name
-    cp -rp $dir /usr/src/app/node_modules/$dir_name
+    rm -rf /storj/bridge/node_modules/$dir_name
+    cp -rp $dir /storj/bridge/node_modules/$dir_name
   fi
 
   echo "Rebuilding linked modules"
